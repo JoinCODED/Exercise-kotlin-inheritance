86b42c412a223d2a919b046906c3dc570b9f69f7
Njood <nejood03@gmail.com>
2025-02-17 18:29:22 +0300

    part 1 and 2 of inheritence has been updated as i previously forgot to add the main function

--- Inheritence/src/Library/book.kt
+++ Inheritence/src/Library/book.kt
@@ -1,9 +1,12 @@
 package Library
 open class Book(val title: String, val author: String, val ISBN: String) {
 
-    // Method to display book information
-    fun displayInfo() {
+    open fun displayInfo() {
         println("$title ($author) - $ISBN ")
     }
 }
 
+fun main() {
+    val book = Book("Tomorrow and Tomorrow and Tomorrow", "Gabrielle Zevin", "1234567890")
+    book.displayInfo()
+}
