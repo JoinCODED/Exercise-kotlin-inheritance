1b7991670a8593310459e7fdc567178df93e0421
Njood <nejood03@gmail.com>
2025-02-17 19:13:52 +0300

    modified FictionBook.kt because i removed a function

--- Inheritence/src/Library/FictionBook.kt
+++ Inheritence/src/Library/FictionBook.kt
@@ -3,9 +3,6 @@ package Library
 class FictionBook(
     title: String, author: String, ISBN: String, val genre: String) : Book(title, author, ISBN) {
 
-    fun getGenre() {
-        println("Genre: $genre")
-    }
 
     override fun displayInfo() {
         super.displayInfo()
@@ -13,9 +10,8 @@ class FictionBook(
     }
 }
 fun main() {
-    val fictionBook = FictionBook("Tomorrow and Tomorrow and Tomorrow", "Gabrielle Zevin", "1234567890", "Fiction")
+    val fictionBook = FictionBook("Tomorrow and Tomorrow and Tomorrow", "Gabrielle Zevin", "9780593321201 ", "Fiction")
 
     fictionBook.displayInfo()
-    fictionBook.getGenre()
 }
 
