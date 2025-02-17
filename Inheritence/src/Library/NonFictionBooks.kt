88f6ada54198cb4b4900634d9a12b506c4de873b
Njood <nejood03@gmail.com>
2025-02-17 18:31:59 +0300

    Step 4: Creating the NonFictionBook Subclass is complete

--- Inheritence/src/Library/NonFictionBooks.kt
+++ Inheritence/src/Library/NonFictionBooks.kt
@@ -1,2 +1,17 @@
 package Library
 
+class NonFictionBook( title: String,author: String, ISBN: String, val fictionType: String) : Book(title, author, ISBN) {
+    fun getFictionType() {
+    }
+
+    override fun displayInfo() {
+        super.displayInfo()
+        println("Fiction Type: $fictionType")
+    }
+}
+fun main() {
+    val nonFictionBook = NonFictionBook("Atomic Habits", "James Clear", "9780735211292", "Self-Help")
+
+    nonFictionBook.displayInfo()
+    nonFictionBook.getFictionType()
+}
