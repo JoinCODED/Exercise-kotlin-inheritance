36e33509e21c0156e700f6c3bd8854511a6d6043
Njood <nejood03@gmail.com>
2025-02-17 19:14:27 +0300

    step 5 is complete

--- Inheritence/src/Library/Main.kt
+++ Inheritence/src/Library/Main.kt
@@ -1,2 +1,12 @@
 package Library
 
+fun main() {
+    val fictionBook = FictionBook("Tomorrow and Tomorrow and Tomorrow", "Gabrielle Zevin", "9780593321201", "Fiction")
+    fictionBook.displayInfo()
+
+
+    val nonFictionBook = NonFictionBook("Atomic Habits", "James Clear", "9780735211292", "Self-Help")
+
+    nonFictionBook.displayInfo()
+    nonFictionBook.getFictionType()
+}
